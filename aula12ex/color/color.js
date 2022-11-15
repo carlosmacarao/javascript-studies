4a9cf98da929641c5c5122d90bde17a44f4bdc90
Carlos Macarao <82442020+carlosmacarao@users.noreply.github.com>
2022-11-15 21:09:33 +0200

    subindo atualizações

--- aula12ex/color/color.js
+++ aula12ex/color/color.js
@@ -1,10 +1,17 @@
 function change() {
+    var agora = new Date()
+    let hora = agora.getHours()
     let bg = document.querySelector('input#color')
+    let res = document.getElementById('res');
 
     let backg = (bg.value)
 
     let body = document.body;
 
     body.style.background = backg;
+
+    if (hora >= 21) {
+        res.innerHTML = `<h3> Olá Carlos </h3>`
+    }
 
 }
